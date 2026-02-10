ae8bdbb388c163026accb4af8c89868d18bee98a
Hrushikesh Ramilla <ramillahrushikesh@gmail.com>
2026-02-10 23:20:17 +0530

    quick fix: handle partial write during append

--- src/wal.cpp
+++ src/wal.cpp
@@ -1,4 +1,3 @@
-// WIP: Need to trace edge cases here (id: 7282)
 #include "wal.h"
 #include "crc32.h"
 
@@ -135,3 +134,5 @@ std::vector<WALEntry> WAL::replay() const {
     wal_close(rfd);
     return entries;
 }
+
+// partial state 2438
