65ef5efc280b739ddd1a1bc72acc7d6ee9598578
hrushikeshramilla <ramillahrushikesh@gmail.com>
2026-03-25 20:22:00 +0530

    feat(concurrency): add background thread pool definition

---
+++ include/thread_pool.h
@@ -0,0 +1,41 @@
+#ifndef ACDB_THREAD_POOL_H
+#define ACDB_THREAD_POOL_H
+
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+
+namespace acdb {
+
+// A lightweight, C++20 thread pool using std::jthread.
+// Dispatches async tasks, used primarily for background flushes and compactions.
+class ThreadPool {
+public:
+    explicit ThreadPool(size_t num_threads);
+    ~ThreadPool();
+
+    // Disable copy/move
+    ThreadPool(const ThreadPool&) = delete;
+    ThreadPool& operator=(const ThreadPool&) = delete;
+
+    // Enqueue a task to be executed by a worker thread
+    void enqueue(std::function<void()> task);
+
+    // Stop all background threads immediately
+    void stop();
+
+private:
+    void worker_loop(std::stop_token stoken);
+
+    std::vector<std::jthread> workers_;
+    std::queue<std::function<void()>> tasks_;
+    std::mutex queue_mutex_;
+    std::condition_variable_any cv_; // Used with jthread's stop_token
+};
+
+} // namespace acdb
+
+#endif // ACDB_THREAD_POOL_H
